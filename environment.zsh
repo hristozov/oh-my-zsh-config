e7752e7577728de0f6c40fff28ecd9c89868b7d0
Georgi Hristozov <georgi@forkbomb.nl>
2013-10-30 14:29:58 +0200

    Cleaned up some old environment variables.

--- environment.zsh
+++ environment.zsh
@@ -1,11 +1,7 @@
-export PS1="%B%F{green}%n@%m%k %B%F{blue}%~ %# %b%f%k"
-export PAGER="/usr/bin/most" #set in env.d
-export CCACHE_DIR="/var/tmp/ccache"
+export PAGER="/usr/bin/most"
 export HISTFILE=~/.histfile
 export HISTSIZE=100000
 export SAVEHIST=100000
-export C_INCLUDE_PATH="/usr/include/libowfat/"
-export CCACHE_SLOPPINESS="time_macros"
-export PATH="/opt/vmware/sbin/:/opt/vmware/lib/vmware/bin/:/opt/bin:/usr/games/bin:/usr/lib64/ccache/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/home/gh/sbt/bin:${PATH}"
+export PATH="/home/gh/idea/bin:${PATH}"
 export IDEA_JDK=/usr/java/jdk1.7.0_40/
 
