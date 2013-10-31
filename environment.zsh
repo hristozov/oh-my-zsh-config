0f2acbf1bd55d09562a7c26e3d6b25732c8567ee
Georgi Hristozov <georgi@forkbomb.nl>
2013-10-31 10:31:34 +0200

    Added ~/bin to PATH.

--- environment.zsh
+++ environment.zsh
@@ -2,6 +2,6 @@ export PAGER="/usr/bin/most"
 export HISTFILE=~/.histfile
 export HISTSIZE=100000
 export SAVEHIST=100000
-export PATH="/home/gh/idea/bin:${PATH}"
+export PATH="/home/gh/bin:/home/gh/idea/bin:${PATH}"
 export IDEA_JDK=/usr/java/jdk1.7.0_40/
 
