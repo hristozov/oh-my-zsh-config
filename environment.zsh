21bafb52c694afd0ede2bbd5f40f0ff39f8c5a3d
Georgi Hristozov <georgi@forkbomb.nl>
2014-02-25 00:08:58 +0200

    Added "git lg".

--- environment.zsh
+++ environment.zsh
@@ -3,4 +3,5 @@ export HISTFILE=~/.histfile
 export HISTSIZE=100000
 export SAVEHIST=100000
 export PATH="/home/gh/bin:/home/gh/idea/bin:/home/gh/dragondisk:${PATH}"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
