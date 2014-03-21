58235f64fb2dbe3b5d91c2c9499d35a806e13e19
Georgi Hristozov <georgi@forkbomb.nl>
2014-03-21 16:48:46 +0200

    Separated the git-specific initialization stuff.

--- environment.zsh
+++ environment.zsh
@@ -6,6 +6,4 @@ export HISTSIZE=100000
 export SAVEHIST=100000
 path+="/home/gh/bin:/home/gh/idea/bin:/home/gh/dragondisk"
 export PATH
-# See https://coderwall.com/p/euwpig
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
---
+++ git.zsh
@@ -0,0 +1,2 @@
+# See https://coderwall.com/p/euwpig
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
