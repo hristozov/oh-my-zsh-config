2ad229bee9b262ecdebbe3afa1b37f36f8d314dc
Georgi Hristozov <georgi@forkbomb.nl>
2014-03-21 16:51:44 +0200

    Initialize the git user information every time a shell is spawned.

--- git.zsh
+++ git.zsh
@@ -1,2 +1,7 @@
+# Just set them every time, cos I'm too lazy to run them every time in a new
+# environment.
+git config --global user.name "Georgi Hristozov"
+git config --global user.email "georgi@forkbomb.nl"
+
 # See https://coderwall.com/p/euwpig
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
