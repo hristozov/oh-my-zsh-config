a5a083ca822322ea471ed32725a1709335489056
Georgi Hristozov <georgi@forkbomb.nl>
2014-06-10 16:16:06 +0300

    Added "commit.gpgsign".

--- git.zsh
+++ git.zsh
@@ -2,6 +2,7 @@
 # environment.
 git config --global user.name "Georgi Hristozov"
 git config --global user.email "georgi@forkbomb.nl"
+git config --global commit.gpgsign
 
 # See https://coderwall.com/p/euwpig
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
