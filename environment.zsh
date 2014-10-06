8d8aa4c563d945bff749b23a71519800b42f550e
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-06 11:42:48 +0300

    Tidying up.

--- environment.zsh
+++ environment.zsh
@@ -1,12 +1,13 @@
 # Various environment variables.
 
+export EDITOR=`which vim`
 export PAGER=`which most`
 export HISTFILE=~/.histfile
 export HISTSIZE=100000
 export SAVEHIST=100000
+
 path+="$HOME/bin"
 path+="$HOME/idea/bin"
 path+="/usr/local/sbin"
 path+="/Applications/Racket v6.0.1/bin"
 export PATH
-export EDITOR=`which vim`
