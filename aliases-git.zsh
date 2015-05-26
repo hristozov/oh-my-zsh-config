e6c5b3bb4d789bb9d045d6cfb9f858af58ddae0a
Georgi Hristozov <georgi@forkbomb.nl>
2015-05-26 15:46:20 +0300

    Add an alias for undoing the last commit.

--- aliases-git.zsh
+++ aliases-git.zsh
@@ -7,6 +7,7 @@ alias gmm='git merge master'
 compdef _git gmm=git-merge
 alias gms='git merge --squash'
 compdef _git gms=git-merge
+alias grs='git reset --soft HEAD~1'
 
 gb-merged() {
   git branch -M $1 "_merged-$1"
