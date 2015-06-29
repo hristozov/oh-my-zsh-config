4b57d01593fa7218fd421dd2f5e9343477d887c3
Georgi Hristozov <georgi@forkbomb.nl>
2015-06-29 16:14:44 +0300

    Change the "gb-merged" logic.

--- plugins/my-git-aliases/my-git-aliases.plugin.zsh
+++ plugins/my-git-aliases/my-git-aliases.plugin.zsh
@@ -9,7 +9,7 @@ alias gms='git merge --squash'
 alias grs='git reset --soft HEAD~1'
 
 gb-merged() {
-  git branch -M $1 "_merged-$1"
+  git branch -M $1 "merged/$1"
 }
 
 # TODO: Move it to a completions file.
