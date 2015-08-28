cc14bb46e60e8236ed67682e9eb23a4f637cc086
Georgi Hristozov <georgi@forkbomb.nl>
2015-08-28 12:56:03 +0300

    Conditionally set PAGER.

--- environment.zsh
+++ environment.zsh
@@ -1,7 +1,13 @@
 # Various environment variables.
 
 export EDITOR=`which vim`
-export PAGER=`which most`
+
+if hash most 2>/dev/null; then
+  export PAGER=`which most`
+else
+  export PAGER=`which less`
+fi
+
 export HISTFILE=~/.histfile
 export HISTSIZE=100000
 export SAVEHIST=100000
