9e704f28a32819caf48e33185ad65d163fa61d0d
Georgi Hristozov <georgi@forkbomb.nl>
2016-02-17 14:17:24 +0200

    Wow such loss :(

--- doge.zsh
+++ doge.zsh
@@ -1,5 +1,5 @@
 # Show a doge if it's available. Requires the appropriate pip module.
 
-if hash doge 2>/dev/null && [ -z "$TMUX" ];  then
-  doge
-fi
+#if hash doge 2>/dev/null && [ -z "$TMUX" ];  then
+#  doge
+#fi
