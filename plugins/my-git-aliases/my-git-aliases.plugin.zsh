11438a3b8cd9ac8c1b5d50f5803821795d276148
Georgi Hristozov <georgi@forkbomb.nl>
2016-07-11 16:53:18 +0300

    Add "gdsst".

--- plugins/my-git-aliases/my-git-aliases.plugin.zsh
+++ plugins/my-git-aliases/my-git-aliases.plugin.zsh
@@ -11,6 +11,7 @@ alias gdm='git diff master'
 alias gdmh='git diff --stat master HEAD'
 alias gdms='git diff --stat master'
 alias gds='git diff --staged'
+alias gdsst='git diff --staged --stat'
 alias gdst='git diff --stat'
 alias git-ignore-file='git update-index --assume-unchanged'
 alias git-list-biggest-objects="git rev-list --objects --all | grep \"\$(git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -10 | awk '{print\$1}')\""
