284b3c1a9033cbfacc3499d7e03809ec4f5c0443
Georgi Hristozov <georgi@forkbomb.nl>
2016-09-09 11:21:08 +0300

    Add "gh".

--- plugins/my-git-aliases/my-git-aliases.plugin.zsh
+++ plugins/my-git-aliases/my-git-aliases.plugin.zsh
@@ -13,6 +13,7 @@ alias gdms='git diff --stat master'
 alias gds='git diff --staged'
 alias gdsst='git diff --staged --stat'
 alias gdst='git diff --stat'
+alias gh='git show'
 alias git-ignore-file='git update-index --assume-unchanged'
 alias git-list-biggest-objects="git rev-list --objects --all | grep \"\$(git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -10 | awk '{print\$1}')\""
 alias git-list-ignored-files="git ls-files -v | grep '^[[:lower:]]'"
