2894f0b5f6d6f8c43eb6f6a74d00b54438f0a2c3
Georgi Hristozov <georgi@forkbomb.nl>
2021-11-11 14:26:04 +0200

    Do not hard-code `master`. Use `$(git_main_branch)` instead.

--- plugins/my-git-aliases/my-git-aliases.plugin.zsh
+++ plugins/my-git-aliases/my-git-aliases.plugin.zsh
@@ -5,13 +5,13 @@ alias gap='git add -p'
 alias gb-remove-merged='git branch --list | egrep "\s{2}merged" | xargs git branch -D'
 alias gbd='git branch -D'
 alias gcob='git checkout -b'
-alias gcom='git checkout origin/master'
+alias gcom='git checkout origin/$(git_main_branch)'
 alias gcs='git changeset'
 alias gdh='git diff head'
-alias gdm='git diff master'
-alias gdmb='git diff $(git merge-base HEAD origin/master)..'
-alias gdmh='git diff --stat master HEAD'
-alias gdms='git diff --stat master'
+alias gdm='git diff $(git_main_branch)'
+alias gdmb='git diff $(git merge-base HEAD origin/$(git_main_branch))..'
+alias gdmh='git diff --stat $(git_main_branch) HEAD'
+alias gdms='git diff --stat $(git_main_branch)'
 alias gdo='git diff origin/$(git_current_branch)'
 alias gdot='git diff origin/$(git_current_branch) --stat'
 alias gds='git diff --staged'
@@ -23,26 +23,26 @@ alias git-ignore-file='git update-index --assume-unchanged'
 alias git-list-biggest-objects="git rev-list --objects --all | grep \"\$(git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -10 | awk '{print\$1}')\""
 alias git-list-ignored-files="git ls-files -v | grep '^[[:lower:]]'"
 alias git-prune-origin='git remote update origin --prune'
-alias git-prune-reflog='git reflog expire --expire=1.minute refs/heads/master && git fsck --unreachable && git prune && git gc'
+alias git-prune-reflog='git reflog expire --expire=1.minute refs/heads/$(git_main_branch) && git fsck --unreachable && git prune && git gc'
 alias git-set-default-branch='git symbolic-ref HEAD refs/heads/'
 alias git-unignore-file='git update-index --no-assume-unchanged'
-alias glmb='git log $(git merge-base HEAD origin/master)..'
-alias glom='git pull origin master'
+alias glmb='git log $(git merge-base HEAD origin/$(git_main_branch))..'
+alias glom='git pull origin $(git_main_branch)'
 alias gmb='git merge-base HEAD'
-alias gmbo='git merge-base HEAD origin/master'
-alias gmm='git merge master'
-alias gmo='git merge origin/master'
+alias gmbo='git merge-base HEAD origin/$(git_main_branch)'
+alias gmm='git merge $(git_main_branch)'
+alias gmo='git merge origin/$(git_main_branch)'
 alias gms='git merge --squash'
 alias gpo='git push origin'
 alias gposu='git push --set-upstream origin $(git_current_branch)'
 alias gpt='git push --tags'
-alias grbm='git rebase master'
-alias grbmi='git rebase -i master'
-alias grbo='git rebase origin/master'
-alias grboi='git rebase -i origin/master'
+alias grbm='git rebase $(git_main_branch)'
+alias grbmi='git rebase -i $(git_main_branch)'
+alias grbo='git rebase origin/$(git_main_branch)'
+alias grboi='git rebase -i origin/$(git_main_branch)'
 alias gre='git recent'
 alias gres='git reset --soft HEAD~1'
-alias grmbi='git rebase -i $(git merge-base HEAD origin/master)'
+alias grmbi='git rebase -i $(git merge-base HEAD origin/$(git_main_branch))'
 alias grs='git restore --staged'
 alias gsui='git submodule update --init'
 alias gt='git tag'
@@ -67,8 +67,8 @@ grbh() {
 git-rebase-changeset() {
   cln=$(git changeset show | head -n1 | awk '{print $2}')
   echo "Rebasing $cln"
-  git rebase $@ origin/master
-  git changeset update --since origin/master $cln
+  git rebase $@ origin/$(git_main_branch)
+  git changeset update --since origin/$(git_main_branch) $cln
 }
 
 gbdd() {
@@ -77,7 +77,7 @@ gbdd() {
 }
 
 git-fix-copyrights() {
-  base=$(git merge-base HEAD origin/master)
+  base=$(git merge-base HEAD origin/$(git_main_branch))
   current_year=$(date +%Y)
   last_year=$(($current_year - 1))
 
