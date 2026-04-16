6aeb134d0fbe045c23180949d6a8e0d817cabbd7
Georgi Hristozov <georgi@forkbomb.nl>
2026-04-16 11:44:19 +0300

    One more path for `nvm`

--- nvm.zsh
+++ nvm.zsh
@@ -1,4 +1,5 @@
 # Load homebrew-provided nvm
 
 export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"
